23b067b92a9f261a3bd11a6cc4f177b0ea5fcdee
Jinpyo <tothejp@gmail.com>
2016-04-14 16:38:55 -0500

    test1`

--- tmp/cloud/client/Callback.java
+++
@@ -1,9 +0,0 @@
-package cloud.client;
-import java.util.HashMap;
-import cloud.client.Connection;
-import cloud.client.ReceiverCallback;
-
-interface Callback{
-	public void start(Connection c);
-	public void response(int requestID, int tag, ReceiverCallback rc,HashMap<String,String> response);
-}
--- tmp/cloud/client/ReceiverCallback.java
+++
@@ -1,7 +0,0 @@
-package cloud.client;
-import java.util.HashMap;
-
-interface ReceiverCallback{
-	public void receive(int requestID,int tag,HashMap<String,String> data);
-	public HashMap<String,String> preReceive(int requestID,int tag,HashMap<String,String> data);
-}
